69cf7e2ae3dd5eb3a5bd209f47a3063ba8f315fb
Philip Maphey <pamaph20@stlawu.edu>
2024-07-14 11:44:35 +0100

    hide env

--- db_test.mjs
+++ db_test.mjs
@@ -62,8 +62,15 @@ app.get("/search/:id", async (request, response) => {
 }); 
 
 app.post("/add_new/:user_id/:id", async (req, res) => {
+  /**
+   * Add new book to a users library
+   */
   const book_id = req.params.id;
   const user_id = req.params.user_id; 
+  if(await check_user(user_id) == 0){
+    res.status(401)
+    res.json(`User ${user_id} does not exist`)
+  }
   const search_uri = `https://openlibrary.org/search.json?q=${book_id}`;
   let json = await getData(search_uri);
   json["user_id"] = user_id;
